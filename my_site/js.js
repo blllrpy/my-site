1428eb3fc5a3b66ee407913d5e94a7d87578a034
blllrpy <99539706+blllrpy@users.noreply.github.com>
2022-09-02 21:43:46 +0300

    yes

--- my_site/js.js
+++ my_site/js.js
@@ -11,3 +11,11 @@ nav.addEventListener("click", function onclick(event) {
   console.log("test");
   documents.body.style.color = generateRandomColor();
 });
+const express = require('express' 4.16.3);
+const app = express();
+
+app.get('/', (request, response) {
+    response.send('Hello World!));
+});
+
+app.listen(3000, console.log('App Listening to port 3000');
